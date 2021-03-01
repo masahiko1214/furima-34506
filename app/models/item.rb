358ebb7c73fe3a67a55746dec74225e5432dc233
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-03-01 11:21:11 +0900

    ActiveHash
    ActiveHashをインストール及びActiveHashに紐づくモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,46 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :user
+    validates :price
+
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+
+  end
+
+
+  
+
+
+
+  belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :burden
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :days_to_ship
+
+
+
+
+  
 end
