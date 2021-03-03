2657f997dab6617f9c309e529afd81e20dfecb62
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-03-03 15:21:59 +0900

    編集ページ調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,17 +21,42 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
+
 
   def edit
-    @item = Item.find(params[:id])
 
-    
   end
+
+  def update
+
+     if  @item.update(item_params)
+      redirect_to item_path(@item.id)
+
+     else
+
+      render :edit
+
+     end
+
+  end
+
+
 
 
   private
+
+  def  move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+ 
+  
+def set_item
+  @item = Item.find(params[:id])
+end
+
 
   def item_params
     params.require(:item).permit(:name, :explanation, :text, :category_id, :condition_id, :burden_id, :prefecture_id,
