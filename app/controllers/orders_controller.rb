f533fd41f57210e16066e1e7b1d890125abb278c
masahiko1214 <78416024+masahiko1214@users.noreply.github.com>
2021-03-07 17:56:47 +0900

    購入フォームの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,15 @@
 class OrdersController < ApplicationController
 
 def index
+  @item = Item.find(params[:item_id])
   @order_shipping_address = OrderShippingAddress.new
 
 end
+
+def create
+  binding.pry
+
+end
+
 
 end
